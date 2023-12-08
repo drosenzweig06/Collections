cded65c483198b5a206042d9704cac7c310fae04
Daniel <drosenzweig06@gmail.com>
2023-12-08 13:16:56 -0800

    //

---
+++ MyHeap.java
@@ -0,0 +1,100 @@
+import java.util.Arrays;
+/**
+ * Write a description of class MyHeap here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class MyHeap<E extends Comparable<E>>
+{
+    // instance variables - replace the example below with your own
+    private E[] elemArray;
+    private int lastNode;
+
+    /**
+     * Constructor for objects of class MyHeap
+     */
+    public MyHeap()
+    {
+        elemArray = (E[]) new Comparable[16];
+        lastNode = -1;
+    }
+    
+    public boolean isEmpty() {
+        return(lastNode == -1);
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public void add(E element)
+    {
+        if (isEmpty()) {
+            elemArray[0] = element;
+            lastNode = 0; 
+        } else {
+            elemArray[lastNode + 1] = element;
+            lastNode++;
+            int child = lastNode;
+            int parent = (child - 1)/2;
+            while(elemArray[child].compareTo(elemArray[parent]) < 0) {
+                elemArray[child] = elemArray[parent];
+                elemArray[parent] = element;
+                child = parent;
+                parent = (child - 1)/2;
+            }
+        }
+    }
+    
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public E getMin()
+    {
+        if(isEmpty()) {
+            return null;
+        } else {
+            return elemArray[0];
+        }
+    }
+    
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public E removeMin()
+    {
+        E toReturn = getMin();
+        return toReturn;
+    }
+    
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public int size()
+    {
+        return lastNode + 1;
+    }
+    
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public String toString()
+    {
+        return Arrays.toString(elemArray);
+    }
+}
---
+++ MyHeapTest.java
@@ -0,0 +1,24 @@
+
+/**
+ * Write a description of class MyHeapTest here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class MyHeapTest
+{
+    public void test() {
+        MyHeap<Integer> test = new MyHeap<Integer>();
+        System.out.println(test.isEmpty());
+        System.out.println(test.getMin());
+        System.out.println(test.size());
+        test.add(15);
+        test.add(10);
+        test.add(5);
+        test.add(20);
+        System.out.println(test.isEmpty());
+        System.out.println(test.getMin());
+        System.out.println(test.size());
+        System.out.println(test.toString());
+    }
+}
